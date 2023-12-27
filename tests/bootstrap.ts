f67bf91e51b0dac31be0d88b29c6cbc2855d3641
Harminder Virk <virk.officials@gmail.com>
2023-12-27 13:10:20 +0530

    feat: add japa/api-client plugin

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -1,4 +1,5 @@
 import { assert } from '@japa/assert'
+import { apiClient } from '@japa/api-client'
 import app from '@adonisjs/core/services/app'
 import type { Config } from '@japa/runner/types'
 import { pluginAdonisJS } from '@japa/plugin-adonisjs'
@@ -12,7 +13,7 @@ import testUtils from '@adonisjs/core/services/test_utils'
  * Configure Japa plugins in the plugins array.
  * Learn more - https://japa.dev/docs/runner-config#plugins-optional
  */
-export const plugins: Config['plugins'] = [assert(), pluginAdonisJS(app)]
+export const plugins: Config['plugins'] = [assert(), apiClient(), pluginAdonisJS(app)]
 
 /**
  * Configure lifecycle function to run before and after all the
