7d9a6288f8bee41ba2d8b5739d3a8631e125aaa1
Densetsuuu <103574286+densetsuuu@users.noreply.github.com>
2025-07-29 17:23:33 +0200

    fix(env): added silent in env.ts LOG_LEVEL enum

--- start/env.ts
+++ start/env.ts
@@ -16,5 +16,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent']),
 })
